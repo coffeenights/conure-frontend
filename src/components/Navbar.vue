e11f63dcf15effe7e35b1351c8677ce76ee77998
mredvard <1638026+mredvard@users.noreply.github.com>
2023-09-19 17:51:32 +0800

    feat: Add navigation breadcrumbs

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -14,8 +14,19 @@
                 </svg>
             </div>
             <div class="breadcrumbs">
-                <div class="border border-gray-200 rounded-lg mt-1 p-2">
-                    <label class="">Conure Project</label>
+                <div class="cursor-pointer border border-gray-200 rounded-lg mt-1 p-2 hover:border-gray-500 transition duration-500 inline-block mr-2">
+                    <label class="mr-2 cursor-pointer">Organization Name</label>
+                    <i class="bi-chevron-down cursor-pointer"></i>
+                </div>
+                <label class=" text-gray-400 mr-2">/</label>
+                <div class="cursor-pointer border border-gray-200 rounded-lg mt-1 p-2 hover:border-gray-500 transition duration-500 inline-block mr-2">
+                    <label class="mr-2 cursor-pointer">first-app</label>
+                    <i class="bi-chevron-down cursor-pointer"></i>
+                </div>
+                <label class=" text-gray-400 mr-2">/</label>
+                <div class="cursor-pointer border border-gray-200 rounded-lg mt-1 p-2 hover:border-gray-500 transition duration-500 inline-block mr-2">
+                    <label class="mr-2 cursor-pointer">staging</label>
+                    <i class="bi-chevron-down cursor-pointer"></i>
                 </div>
             </div>
             <div class="profile "></div>
