78f65285d525c15063e32b558129289b8fe23d0e
mredvard <1638026+mredvard@users.noreply.github.com>
2023-10-18 17:07:21 +0800

    feat: Add pinia

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import { RouteRecordRaw, createRouter, createWebHistory } from 'vue-router'
+import { createPinia } from 'pinia'
 import './styles/style.css'
 import App from './App.vue'
 import PageNotFound from  './views/404.vue'
@@ -45,8 +46,8 @@ const router = createRouter({
     routes, 
 })
 
-
+// const pinia = createPinia()
 const app = createApp(App);
 app.use(router);
-//app.use(axios);
+// app.use(pinia);
 app.mount('#app');
