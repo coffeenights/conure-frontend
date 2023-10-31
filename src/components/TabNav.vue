983aa75a8991502c04b0924373a5cbee1b933b02
mredvard <1638026+mredvard@users.noreply.github.com>
2023-10-31 16:34:15 +0800

    feat: Separate the tabs header into a component

--- src/components/TabNav.vue
+++ src/components/TabNav.vue
@@ -1,5 +1,31 @@
+<script setup lang="ts">
+import { useRouter, useRoute } from 'vue-router'
+import { ref } from 'vue'
+
+const router = useRouter()
+const route = useRoute()
+
+interface Tab {
+    routeName: string
+    caption: string
+}
+const props = defineProps<{
+  tabs: Tab[]
+  default: string
+}>()
+const tabItem = ref(props.default)
+
+function setTab(tab: string) {
+    tabItem.value = tab
+    router.push({name: tab})
+}
+function isActive(tab: string): boolean {
+    return tabItem.value == tab
+}
+</script>
+
 <template>
-    <slot name="header"> 
-        
-    </slot>
+    <div id="tabsHeader" class="flex gap-2 border-b mt-3 border-color">
+        <div v-for="tab in props.tabs" @click="setTab(tab.routeName)" class="mb-[-1px] p-3 cursor-pointer" :class="{'tab-active': isActive(tab.routeName)}">{{ tab.caption }}</div>
+    </div>
 </template>
--- src/views/applications/ApplicationDetails.vue
+++ src/views/applications/ApplicationDetails.vue
@@ -1,29 +1,21 @@
 <script setup lang="ts">
 import DetailsHeader from "../../components/DetailsHeader.vue";
-import { useRouter, useRoute } from 'vue-router'
-import { ref } from 'vue'
+import TabNav from "../../components/TabNav.vue";
 
-const router = useRouter()
-const route = useRoute()
-const tabItem = ref('componentList')
-function setTab(tab: string) {
-    tabItem.value = tab
-    router.push({name: tab})
-}
-function isActive(tab: string): boolean {
-    return tabItem.value == tab
-}
+const tabs = [
+    { routeName: 'componentList', caption: 'Components' },
+    { routeName: 'applicationDetailsVariables', caption: 'Variables' },
+    { routeName: 'users', caption: 'Users' },
+    { routeName: 'activity', caption: 'Activity' },
+] 
+
+const defaultTab = 'componentList'
 </script>
 
 <template>
     <div class="content-wrapper p-4 flex flex-col justify-stretch">
         <DetailsHeader />
-        <div id="tabsHeader" class="flex gap-2 border-b mt-3 border-color">
-            <div @click="setTab('componentList')" class="mb-[-1px] p-3 cursor-pointer" :class="{'tab-active': isActive('componentList')}">Components</div>
-            <div @click="setTab('applicationDetailsVariables')" class="mb-[-1px] p-3 hover:tab-active cursor-pointer" :class="{'tab-active': isActive('applicationDetailsVariables')}">Variables</div>
-            <div @click="setTab('users')" class="mb-[-1px] p-3 hover:tab-active cursor-pointer" :class="{'tab-active': isActive('users')}">Users</div>
-            <div @click="setTab('activity')" class="mb-[-1px] p-3 hover:tab-active cursor-pointer" :class="{'tab-active': isActive('activity')}">Activity</div>
-        </div>
+        <TabNav :tabs=tabs :default=defaultTab />
         <div id="tabsWrapper" class="mt-3 grow flex flex-col">
             <router-view></router-view>
         </div>
