386889146fd16f5e77b8bbce80b6dcd87ec1d6e3
mredvard <1638026+mredvard@users.noreply.github.com>
2024-02-22 14:59:08 +0800

    chore: remove unused code

--- src/stores/BreadCrumbStore.ts
+++ src/stores/BreadCrumbStore.ts
@@ -5,22 +5,10 @@ import { ref } from 'vue';
 
 export const useBreadCrumbStore = defineStore('BreadCrumbStore', () => {
     const environment = ref('')
-    // TODO: start with some default values for testing
     const applicationId = ref('')
     const application = ref('')
     const organizationId = ref('')
-    const organization = ref('ConureTest')
-
-    // appId 65c084eaff5e41522cc80f99
-    // orgId 6599082303bedbfeb7243ada
-
-    function updateBreadcrumbState(appId: string, orgId: string, env: string) {
-        
-        applicationId.value = appId
-        organizationId.value = orgId
-        environment.value = env
-    }
-    
+    const organization = ref('')
 
     return { 
         environment, 
