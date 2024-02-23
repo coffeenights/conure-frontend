aede1f8b8c61e4b72f157fd44146dc10b7e1342d
Edgar Contreras <1638026+mredvard@users.noreply.github.com>
2024-02-23 16:10:32 +0800

    feat: Integrate shadcn-vue and add a sample component "Card"

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import { fileURLToPath, URL } from "url";
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
@@ -6,6 +8,7 @@ export default defineConfig({
   plugins: [vue()],
   resolve: {
     alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
     }
   },
 })
