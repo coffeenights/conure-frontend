3dc539edd45dbc76fda8666bbc01e246da43b773
Oscar Lotero <oscarelotero@gmail.com>
2024-03-13 18:31:18 +0100

    Apply format (prettier) to 404 and Empty

--- src/views/EmptyState.vue
+++ src/views/EmptyState.vue
@@ -1,8 +1,8 @@
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
 <template>
-    <div class="flex flex-col items-center">
-        <h1 class="text-3xl dark:text-blue-300 text-blue-900 font-extrabold mt-10">Nothing here yet!</h1>
-    </div>
-
+  <div class="flex flex-col items-center">
+    <h1 class="text-3xl dark:text-blue-300 text-blue-900 font-extrabold mt-10">
+      Nothing here yet!
+    </h1>
+  </div>
 </template>
