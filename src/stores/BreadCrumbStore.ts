e8b1af23d99c3efa02f56a75e9469de766dc700c
Oscar Lotero <oscarelotero@gmail.com>
2024-03-13 18:36:53 +0100

    Apply format (prettier) to stores

--- src/stores/BreadCrumbStore.ts
+++ src/stores/BreadCrumbStore.ts
@@ -1,22 +1,20 @@
-import { defineStore } from "pinia";
-import { list } from "postcss";
-import { ref } from 'vue';
-
+import { defineStore } from 'pinia'
+import { ref } from 'vue'
 
 export const useBreadCrumbStore = defineStore('BreadCrumbStore', () => {
-    const environment = ref('')
-    const applicationId = ref('')
-    const application = ref('')
-    const organizationId = ref('')
-    const organization = ref('')
-    const isLoaded = ref(false)
+  const environment = ref('')
+  const applicationId = ref('')
+  const application = ref('')
+  const organizationId = ref('')
+  const organization = ref('')
+  const isLoaded = ref(false)
 
-    return { 
-        environment, 
-        application,
-        applicationId, 
-        organization,
-        organizationId, 
-        isLoaded
-    }
+  return {
+    environment,
+    application,
+    applicationId,
+    organization,
+    organizationId,
+    isLoaded,
+  }
 })
