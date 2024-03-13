7ff02550f4186d4af9255a605ef12b926133b3f8
Oscar Lotero <oscarelotero@gmail.com>
2024-03-13 18:32:45 +0100

    Apply format (prettier) to components

--- src/components/Breadcrumb.vue
+++ src/components/Breadcrumb.vue
@@ -7,32 +7,46 @@ let organization = ref(store.organization)
 let application = ref(store.application)
 let environment = ref(store.environment)
 
-
-watch(() => store.organization, (newOrganization) => {
+watch(
+  () => store.organization,
+  (newOrganization) => {
     organization.value = newOrganization
-})
-watch(() => store.application, (newApplication) => {
+  },
+)
+watch(
+  () => store.application,
+  (newApplication) => {
     application.value = newApplication
-})
-watch(() => store.environment, (newEnvironment) => {
+  },
+)
+watch(
+  () => store.environment,
+  (newEnvironment) => {
     environment.value = newEnvironment
-})
-
+  },
+)
 </script>
 <template>
-    <div class="cursor-pointer border border-gray-200 dark:border-gray-700 rounded-lg mt-1 p-2 hover:border-gray-500 dark:hover:border-gray-500  transition duration-500 inline-block mr-2">
-        <label class="mr-2 cursor-pointer">{{ organization }}</label>
-    </div>
-    <label class="text-gray-400 dark:text-gray-700 mr-2">/</label>
-    <div class="cursor-pointer border border-gray-200 dark:border-gray-700 rounded-lg mt-1 p-2 hover:border-gray-500 dark:hover:border-gray-500  transition duration-500 inline-block mr-2">
-        <label class="mr-2 cursor-pointer">
-            <i class="bi bi-diagram-3 cursor-pointer pr-1"></i>
-            {{ application }}
-        </label>
-        <i class="bi-chevron-down cursor-pointer"></i>
-    </div>
-    <div v-if="environment" class="cursor-pointer border border-gray-200 dark:border-gray-700 rounded-lg mt-1 p-2 hover:border-gray-500 dark:hover:border-gray-500  transition duration-500 inline-block mr-2">
-        <label class="mr-2 cursor-pointer">{{ environment }}</label>
-        <i class="bi-chevron-down cursor-pointer"></i>
-    </div>
+  <div
+    class="cursor-pointer border border-gray-200 dark:border-gray-700 rounded-lg mt-1 p-2 hover:border-gray-500 dark:hover:border-gray-500 transition duration-500 inline-block mr-2"
+  >
+    <label class="mr-2 cursor-pointer">{{ organization }}</label>
+  </div>
+  <label class="text-gray-400 dark:text-gray-700 mr-2">/</label>
+  <div
+    class="cursor-pointer border border-gray-200 dark:border-gray-700 rounded-lg mt-1 p-2 hover:border-gray-500 dark:hover:border-gray-500 transition duration-500 inline-block mr-2"
+  >
+    <label class="mr-2 cursor-pointer">
+      <i class="bi bi-diagram-3 cursor-pointer pr-1"></i>
+      {{ application }}
+    </label>
+    <i class="bi-chevron-down cursor-pointer"></i>
+  </div>
+  <div
+    v-if="environment"
+    class="cursor-pointer border border-gray-200 dark:border-gray-700 rounded-lg mt-1 p-2 hover:border-gray-500 dark:hover:border-gray-500 transition duration-500 inline-block mr-2"
+  >
+    <label class="mr-2 cursor-pointer">{{ environment }}</label>
+    <i class="bi-chevron-down cursor-pointer"></i>
+  </div>
 </template>
--- src/components/DetailsHeader.vue
+++ src/components/DetailsHeader.vue
@@ -1,18 +1,23 @@
 <template>
-    <div class="flex">
-        <div class="text-lg grow">
-            <router-link :to="{ name: 'applications', params: { organizationId: 'asd' }  }" custom v-slot="{ navigate }">
-                <span @click="navigate" class="cursor-pointer bi-arrow-left mr-3"></span>
-            </router-link>
-            first-app
-        </div>
-        <div class="flex gap-2">
-            <div class="button bg-yellow-600">
-                Deploy
-            </div>
-            <div class="button">
-                <span class="bi-chevron-down"></span>
-            </div>
-        </div>
+  <div class="flex">
+    <div class="text-lg grow">
+      <router-link
+        :to="{ name: 'applications', params: { organizationId: 'asd' } }"
+        custom
+        v-slot="{ navigate }"
+      >
+        <span
+          @click="navigate"
+          class="cursor-pointer bi-arrow-left mr-3"
+        ></span>
+      </router-link>
+      first-app
     </div>
+    <div class="flex gap-2">
+      <div class="button bg-yellow-600">Deploy</div>
+      <div class="button">
+        <span class="bi-chevron-down"></span>
+      </div>
+    </div>
+  </div>
 </template>
