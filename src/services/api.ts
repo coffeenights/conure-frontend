91dcb0aee073bfefa1c72a4b6947c4d34845160f
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-04 14:17:33 +0800

    feat: Add support for browser cookies in axios

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,6 +2,7 @@ import axios, { AxiosInstance } from 'axios'
 
 const instance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
+  withCredentials: true,
 })
 
 export type ApiResponse<Data> = {
