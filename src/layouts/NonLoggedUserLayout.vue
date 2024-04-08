fa20735f66b19d3d0d368a6324911abea8831361
Oscar Lotero <oscarelotero@gmail.com>
2024-04-08 18:43:56 +0200

    Add NonLoggedUserLayout.vue layout file

---
+++ src/layouts/NonLoggedUserLayout.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <section>
+    <div class="flex flex-col items-center pt-16">
+      <router-view></router-view>
+    </div>
+  </section>
+</template>
