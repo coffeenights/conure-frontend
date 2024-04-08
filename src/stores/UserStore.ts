3931fb10cb8874a0b3f620501958a2dff191d7cb
Oscar Lotero <oscarelotero@gmail.com>
2024-04-08 18:43:24 +0200

    Add localstorage to persist the user state

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -1,10 +1,30 @@
 import { defineStore } from 'pinia'
 import { ref } from 'vue'
 
-export const useUserStore = defineStore('UserStore', () => {
-  const isAuthenticated = ref(false)
+const STORE_NAME = 'UserStore'
+
+export const useUserStore = defineStore(STORE_NAME, () => {
+  const authenticated = ref(false)
+
+  const initAuthState = () => {
+    const user = localStorage.getItem(STORE_NAME)
+    authenticated.value = user ? JSON.parse(user) : false
+  }
+
+  initAuthState()
+
+  function login() {
+    authenticated.value = true
+    localStorage.setItem(STORE_NAME, JSON.stringify(true))
+  }
+  function logout() {
+    authenticated.value = false
+    localStorage.setItem(STORE_NAME, JSON.stringify(false))
+  }
 
   return {
-    isAuthenticated,
+    authenticated,
+    login,
+    logout,
   }
 })
