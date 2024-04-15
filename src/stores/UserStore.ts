22e61fd7bbd9a1095816a46663525f80e34e95a4
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-15 17:01:28 +0800

    Load the states at the momento of loading the routes and not at loading the Layout component

--- src/stores/UserStore.ts
+++ src/stores/UserStore.ts
@@ -6,20 +6,11 @@ const STORE_NAME = 'UserStore'
 export const useUserStore = defineStore(STORE_NAME, () => {
   const authenticated = ref(false)
 
-  const initAuthState = () => {
-    const user = localStorage.getItem(STORE_NAME)
-    authenticated.value = user ? JSON.parse(user) : false
-  }
-
-  initAuthState()
-
   function login() {
     authenticated.value = true
-    localStorage.setItem(STORE_NAME, JSON.stringify(true))
   }
   function logout() {
     authenticated.value = false
-    localStorage.setItem(STORE_NAME, JSON.stringify(false))
   }
 
   return {
--- src/views/applications/routes.ts
+++ src/views/applications/routes.ts
@@ -1,5 +1,5 @@
 import EmptyState from '../EmptyState.vue'
-import Applications from './ApplicationList.vue'
+import ApplicationList from './ApplicationList.vue'
 import ApplicationDetails from './ApplicationDetails.vue'
 import ApplicationDetailsVariables from './ApplicationDetailsVariables.vue'
 import ComponentList from './ComponentList.vue'
@@ -13,51 +13,51 @@ const applicationRoutes: Array<RouteRecordRaw> = [
     path: '/organizations/:organizationId',
     component: EmptyState,
     name: 'organization',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresBreadcrumbState: true},
   },
   {
     path: '/organizations/:organizationId/applications',
-    component: Applications,
+    component: ApplicationList,
     name: 'applications',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresBreadcrumbState: true },
   },
   {
     path: '/organizations/:organizationId/applications/:applicationId/:environment',
     component: ApplicationDetails,
     name: 'applicationDetails',
-    meta: { requiresAuth: true },
+    meta: { requiresAuth: true, requiresBreadcrumbState: true },
     redirect: { name: 'componentList' },
     children: [
       {
         path: 'variables',
         component: ApplicationDetailsVariables,
         name: 'applicationDetailsVariables',
-        meta: { requiresAuth: true },
+        meta: { requiresAuth: true, requiresBreadcrumbState: true },
       },
       {
         path: 'components',
         component: ComponentList,
         name: 'componentList',
-        meta: { requiresAuth: true },
+        meta: { requiresAuth: true, requiresBreadcrumbState: true},
         children: [
           {
             path: ':componentName',
             redirect: { name: 'componentDetailsTab' },
             component: ComponentDetails,
             name: 'componentDetails',
-            meta: { requiresAuth: true },
+            meta: { requiresAuth: true, requiresBreadcrumbState: true},
             children: [
               {
                 path: 'details',
                 component: ComponentDetailsTab,
                 name: 'componentDetailsTab',
-                meta: { requiresAuth: true },
+                meta: { requiresAuth: true, requiresBreadcrumbState: true },
               },
               {
                 path: 'settings',
                 component: ComponentSettingsTab,
                 name: 'componentSettingsTab',
-                meta: { requiresAuth: true },
+                meta: { requiresAuth: true, requiresBreadcrumbState: true },
               },
             ],
           },
