58b5c4e4cc8504676b873e245f8c25f3dc3c75ae
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-15 16:59:14 +0800

    Refactor import statements and add authentication checks in main.ts

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,10 @@ import PageNotFound from './views/404.vue'
 import EmptyState from './views/EmptyState.vue'
 import applicationRoutes from './views/applications/routes'
 import authRoutes from './views/auth/routes'
-import { useUserStore } from './stores/UserStore'
+import { useUserStore } from '@/stores/UserStore'
+import { useBreadCrumbStore } from '@/stores/BreadCrumbStore'
+import { detailOrganization, detailApplication } from '@/services/organizations'
+import { authenticationStatus } from '@/services/auth'
 
 const routes: Array<RouteRecordRaw> = [
   {
@@ -30,17 +33,76 @@ const router = createRouter({
   routes,
 })
 
+
+
 router.beforeEach((to, from, next) => {
   const userStore = useUserStore()
+  const breadCrumbStore = useBreadCrumbStore()
   if (
     to.matched.some((record) => record.meta.requiresAuth) &&
     !userStore.authenticated
   ) {
+
+    // check if the user is authenticated against the server
+    authenticationStatus()
+    .then((response) => {
+      console.log(response)
+    })
+    .catch()
     // Redirect to the login page if the user is not authenticated
     const nextUrl = to.fullPath
     next({ path: '/auth/login', query: { next: nextUrl } })
+  } else if (
+    to.matched.some((record) => record.meta.requiresBreadcrumbState) &&
+    !breadCrumbStore.isLoaded
+  ) {
+    let orgResponse: Promise<any> | undefined
+    let appResponse: Promise<any> | undefined
+    if (to.params.organizationId) {
+      orgResponse = detailOrganization(to.params.organizationId as string)
+        .then((response) => {
+          breadCrumbStore.organization = response.data.name
+          breadCrumbStore.organizationId = response.data.id
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            router.push({ name: '404' })
+          } else {
+            throw error
+          }
+        })
+    }
+    if (to.params.applicationId) {
+      appResponse = detailApplication(
+        to.params.organizationId as string,
+        to.params.applicationId as string,
+        to.params.environment as string,
+      )
+        .then((response) => {
+          breadCrumbStore.application = response?.data.name as string
+          breadCrumbStore.applicationId = response?.data.id as string
+        })
+        .catch((error) => {
+          if (error.response.status === 404) {
+            router.push({ name: '404' })
+          } else {
+            throw error
+          }
+        })
+    }
+    if (to.params.environment) {
+      breadCrumbStore.environment = to.params.environment as string
+    }
+    Promise.all([orgResponse, appResponse])
+      .then(() => {
+        breadCrumbStore.isStoreLoaded = true
+      })
+      .catch((error) => {
+        throw error
+      }
+    )
+    next()
   } else {
-    // Proceed as normal if the user is authenticated or the route doesn't require authentication
     next()
   }
 })
