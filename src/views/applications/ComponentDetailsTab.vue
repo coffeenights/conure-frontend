df0d74e8cf7013ba5c61335c051c91e00c19e815
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-17 12:59:15 +0800

    Remove Activity card and prettify the file

--- src/views/applications/ComponentDetailsTab.vue
+++ src/views/applications/ComponentDetailsTab.vue
@@ -13,21 +13,19 @@ import { useBreadCrumbStore } from '@/stores/BreadCrumbStore'
 import { statusComponent } from '@/services/organizations'
 import { useRoute } from 'vue-router'
 
-
 const route = useRoute()
 const componentStore = useComponentStore()
 const breadCrumbStore = useBreadCrumbStore()
 const store = useComponentStore()
 const isLoading = ref<boolean>(true)
 
-
 onMounted(() => {
   statusComponent(
-    breadCrumbStore.organizationId, 
-    breadCrumbStore.applicationId, 
-    breadCrumbStore.environment, 
-    route.params.componentName as string
-    )
+    breadCrumbStore.organizationId,
+    breadCrumbStore.applicationId,
+    breadCrumbStore.environment,
+    route.params.componentName as string,
+  )
     .then((response) => {
       componentStore.componentStatus = response.data
       isLoading.value = false
@@ -35,18 +33,33 @@ onMounted(() => {
     .catch((error) => {
       console.log(error)
     })
-  })
-
+})
 </script>
 <template>
   <div class="flex flex-row gap-2 flex-wrap">
     <div class="grow flex flex-col gap-2 sm:min-w-[22rem] md:min-w-[34rem]">
       <Card class="grow">
         <CardContent class="p-4 grid grid-cols-2 sm:grid-cols-4 gap-2">
-          <CardContentKeyValueVertical cKey="Name" :value="store.componentStatus?.component?.id" :isLoading="isLoading" />
-          <CardContentKeyValueVertical cKey="Type" :value="store.componentStatus?.component?.type" :isLoading="isLoading" />
-          <CardContentKeyValueVertical cKey="Replicas" :value="store.componentStatus?.properties?.resources?.replicas" :isLoading="isLoading" />
-          <CardContentKeyValueVertical cKey="Status" :value="store.componentStatus?.properties?.status" :isLoading="isLoading" />
+          <CardContentKeyValueVertical
+            c-key="Name"
+            :value="store.componentStatus?.component?.id"
+            :is-loading="isLoading"
+          />
+          <CardContentKeyValueVertical
+            c-key="Type"
+            :value="store.componentStatus?.component?.type"
+            :is-loading="isLoading"
+          />
+          <CardContentKeyValueVertical
+            c-key="Replicas"
+            :value="store.componentStatus?.properties?.resources?.replicas"
+            :is-loading="isLoading"
+          />
+          <CardContentKeyValueVertical
+            c-key="Status"
+            :value="store.componentStatus?.properties?.status"
+            :is-loading="isLoading"
+          />
         </CardContent>
       </Card>
       <div class="flex grow gap-2 flex-wrap sm:flex-nowrap">
@@ -57,10 +70,26 @@ onMounted(() => {
             </CardTitle>
           </CardHeader>
           <CardContent class="p-4">
-            <CardContentKeyValue cKey="IP" :value="store.componentStatus?.properties?.network?.ip" :isLoading="isLoading" />
-            <CardContentKeyValue cKey="External IP" :value="store.componentStatus?.properties?.network?.external_ip" :isLoading="isLoading" />
-            <CardContentKeyValue cKey="Host" :value="store.componentStatus?.properties?.network?.host" :isLoading="isLoading" />
-            <CardContentKeyValue cKey="Ports" :value="store.componentStatus?.properties?.network?.port" :isLoading="isLoading" />
+            <CardContentKeyValue
+              c-key="IP"
+              :value="store.componentStatus?.properties?.network?.ip"
+              :is-loading="isLoading"
+            />
+            <CardContentKeyValue
+              c-key="External IP"
+              :value="store.componentStatus?.properties?.network?.external_ip"
+              :is-loading="isLoading"
+            />
+            <CardContentKeyValue
+              c-key="Host"
+              :value="store.componentStatus?.properties?.network?.host"
+              :is-loading="isLoading"
+            />
+            <CardContentKeyValue
+              c-key="Ports"
+              :value="store.componentStatus?.properties?.network?.port"
+              :is-loading="isLoading"
+            />
           </CardContent>
         </Card>
         <Card class="grow min-w-[10rem] w-full">
@@ -70,7 +99,13 @@ onMounted(() => {
             </CardTitle>
           </CardHeader>
           <CardContent class="p-4">
-            <CardContentKeyValue cKey="Image" :value="store.componentStatus?.properties?.source?.container_image" :isLoading="isLoading" />
+            <CardContentKeyValue
+              c-key="Image"
+              :value="
+                store.componentStatus?.properties?.source?.container_image
+              "
+              :is-loading="isLoading"
+            />
           </CardContent>
         </Card>
       </div>
@@ -82,9 +117,21 @@ onMounted(() => {
             </CardTitle>
           </CardHeader>
           <CardContent class="p-4">
-            <CardContentKeyValue cKey="CPU" :value="store.componentStatus?.properties?.resources?.cpu" :isLoading="isLoading" />
-            <CardContentKeyValue cKey="Memory" :value="store.componentStatus?.properties?.resources?.memory" :isLoading="isLoading" />
-            <CardContentKeyValue cKey="Replicas" :value="store.componentStatus?.properties?.resources?.replicas" :isLoading="isLoading" />
+            <CardContentKeyValue
+              c-key="CPU"
+              :value="store.componentStatus?.properties?.resources?.cpu"
+              :is-loading="isLoading"
+            />
+            <CardContentKeyValue
+              c-key="Memory"
+              :value="store.componentStatus?.properties?.resources?.memory"
+              :is-loading="isLoading"
+            />
+            <CardContentKeyValue
+              c-key="Replicas"
+              :value="store.componentStatus?.properties?.resources?.replicas"
+              :is-loading="isLoading"
+            />
           </CardContent>
         </Card>
         <Card class="grow min-w-[10rem] w-full">
@@ -94,22 +141,14 @@ onMounted(() => {
             </CardTitle>
           </CardHeader>
           <CardContent class="p-4">
-            <CardContentKeyValue cKey="Size" :value="store.componentStatus?.properties?.storage?.size" :isLoading="isLoading" />
+            <CardContentKeyValue
+              c-key="Size"
+              :value="store.componentStatus?.properties?.storage?.size"
+              :is-loading="isLoading"
+            />
           </CardContent>
         </Card>
       </div>
-    </div>
-    <div class="grow flex-col min-w-[20rem]">
-      <Card class="grow min-w-[20rem] w-full">
-        <CardHeader class="pl-4 pt-4 pb-0">
-          <CardTitle class="text-lg">
-            <span class="bi-clock-history mr-2"></span>Activity
-          </CardTitle>
-        </CardHeader>
-        <CardContent class="p-4">
-          <div class=""></div>
-        </CardContent>
-      </Card>
     </div>
   </div>
 </template>
