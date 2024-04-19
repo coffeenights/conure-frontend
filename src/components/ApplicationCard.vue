e37f4ad95bae69cc8f702f97b07bd6447b759151
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-19 10:54:10 +0800

    Prettify file

--- src/components/ApplicationCard.vue
+++ src/components/ApplicationCard.vue
@@ -2,21 +2,27 @@
 import { useBreadCrumbStore } from '@/stores/BreadCrumbStore'
 import { useRouter } from 'vue-router'
 import { ref } from 'vue'
-import { Application, Environment, Revision, getTimeAgo, getLatestRevision } from '@/services/organizations'
+import {
+  Application,
+  Environment,
+  Revision,
+  getTimeAgo,
+  getLatestRevision,
+} from '@/services/organizations'
 
 const router = useRouter()
 const store = useBreadCrumbStore()
 const latestRevision = ref<Revision | null>(null)
 
 const props = defineProps({
-    application: {
-        type: Object as () => Application,
-        required: true,
-    },
-    environment: {
-        type: Object as () => Environment,
-        required: true,
-    },
+  application: {
+    type: Object as () => Application,
+    required: true,
+  },
+  environment: {
+    type: Object as () => Environment,
+    required: true,
+  },
 })
 
 latestRevision.value = getLatestRevision(props.application)
@@ -25,7 +31,11 @@ const getFirstLetter = (name: string): string => {
   return name.charAt(0).toUpperCase()
 }
 
-function goToDetailApplication( applicationId: string, applicationName: string, environment: string, ) {
+function goToDetailApplication(
+  applicationId: string,
+  applicationName: string,
+  environment: string,
+) {
   store.applicationId = applicationId
   store.environment = environment
   store.application = applicationName
@@ -42,46 +52,40 @@ function goToDetailApplication( applicationId: string, applicationName: string, 
 </script>
 
 <template>
-<div
-    @click="
-    goToDetailApplication(
-        application.id,
-        application.name,
-        environment.name,
-    )
-    "
+  <div
     class="cursor-pointer border bg-card drop-shadow w-64 shrink-0 rounded-lg flex flex-col overflow-hidden"
->
+    @click="
+      goToDetailApplication(application.id, application.name, environment.name)
+    "
+  >
     <div class="p-4">
-        <div class="text-lg">{{ environment.name }}</div>
-        <div v-if="latestRevision" class="text-xs text-muted-foreground">
-            Last update {{ getTimeAgo(latestRevision.created_at) }} ago
+      <div class="text-lg">{{ environment.name }}</div>
+      <div v-if="latestRevision" class="text-xs text-muted-foreground">
+        Last update {{ getTimeAgo(latestRevision.created_at) }} ago
+      </div>
+      <div v-else class="text-xs text-muted-foreground">No updates yet</div>
+      <div class="border border-t-[1px] border-b-0 mt-9 h-0"></div>
+      <div class="text-xl flex items-center pt-2 pb-2 justify-center">
+        <div class="grow flex items-center justify-center gap-2">
+          <div class="bi-boxes"></div>
+          <span class="text-sm">{{ application.total_components }}</span>
         </div>
-        <div v-else class="text-xs text-muted-foreground">No updates yet</div>
-        <div class="border border-t-[1px] border-b-0 mt-9 h-0"></div>
-        <div class="text-xl flex items-center pt-2 pb-2 justify-center">
-            <div class="grow flex items-center justify-center gap-2">
-            <div class="bi-boxes"></div>
-            <span class="text-sm">{{ application.total_components }}</span>
-            </div>
-            <div
-            class="h-5 w-0 border border-l-0 "
-            ></div>
-            <div class="grow flex items-center justify-center gap-2">
-            <div class="bi-people"></div>
-            <span class="text-sm">3</span>
-            </div>
+        <div class="h-5 w-0 border border-l-0"></div>
+        <div class="grow flex items-center justify-center gap-2">
+          <div class="bi-people"></div>
+          <span class="text-sm">3</span>
         </div>
-        <div
-            class="border border-t-[1px] border-b-0 h-0 "
-        ></div>
-        <div class="flex items-center mt-1 text-muted-foreground">
-            <div class="text-xs grow">Rev. {{ latestRevision.revision_number | 0 }}</div>
-            <div class="text-lime-600">
-            <span class="text-xs bi-circle-fill pr-1"></span>
-            </div>
-            <div class="text-xs">{{ application.status }}</div>
+      </div>
+      <div class="border border-t-[1px] border-b-0 h-0"></div>
+      <div class="flex items-center mt-1 text-muted-foreground">
+        <div class="text-xs grow">
+          Rev. {{ latestRevision.revision_number | 0 }}
         </div>
+        <div class="text-lime-600">
+          <span class="text-xs bi-circle-fill pr-1"></span>
+        </div>
+        <div class="text-xs">{{ application.status }}</div>
+      </div>
     </div>
-</div>
+  </div>
 </template>
