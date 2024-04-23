d6f21552bf906286e241c3aeb9d9fe51d0e7dbd8
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-23 15:47:34 +0800

    Add the Toaster globally

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 import DefaultLayout from './layouts/DefaultLayout.vue'
 import NonLoggedUserLayout from './layouts/NonLoggedUserLayout.vue'
 import { useUserStore } from '@/stores/UserStore'
+import Toaster from '@/components/ui/toast/Toaster.vue'
 
 const userStore = useUserStore()
 </script>
 
 <template>
+  <Toaster />
   <div>
     <DefaultLayout v-if="userStore.authenticated" />
     <NonLoggedUserLayout v-else />
