d36eedc2adb2f2e7390d189cb92b7d44d0e67dd5
mredvard <1638026+mredvard@users.noreply.github.com>
2024-04-24 12:25:04 +0800

    Add a profile icon and button to toggle dark mode

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,11 +1,20 @@
 <script setup lang="ts">
 import Breadcrumb from '../components/Breadcrumb.vue'
 import CompanyLogo from '../components/CompanyLogo.vue'
+import { Button } from '@/components/ui/button'
+import {
+  HoverCard,
+  HoverCardTrigger,
+  HoverCardContent,
+} from '@/components/ui/hover-card'
+import { HoverCardArrow } from 'radix-vue'
+import { Avatar, AvatarFallback } from '@/components/ui/avatar'
+import { useDarkMode } from '@/composables/useDarkMode'
+
+const { isDarkMode, toggleDarkMode } = useDarkMode()
 </script>
 <template>
-  <nav
-    class="bg-card h-16 shadow-sm min-w-max sticky top-0 border-b z-10"
-  >
+  <nav class="bg-card h-16 shadow-sm min-w-max sticky top-0 border-b z-10">
     <div class="flex flex-row items-center">
       <div class="logo mr-6 w-12 ml-3">
         <CompanyLogo />
@@ -13,7 +22,32 @@ import CompanyLogo from '../components/CompanyLogo.vue'
       <div class="breadcrumbs">
         <Breadcrumb />
       </div>
-      <div class="profile"></div>
+      <div class="flex flex-grow justify-end mt-2">
+        <HoverCard>
+          <HoverCardTrigger as-child>
+            <Button
+              size="icon"
+              variant="ghost"
+              class="mr-5"
+              @click="toggleDarkMode"
+            >
+              <span
+                class="text-xl"
+                :class="{ 'bi-moon-stars': isDarkMode, 'bi-sun': !isDarkMode }"
+              ></span>
+            </Button>
+          </HoverCardTrigger>
+          <HoverCardContent class="text-sm w-max bg-black border-0 text-white dark:text-black dark:bg-white">
+            Switch theme
+            <HoverCardArrow class="dark:fill-white" :width="8" />
+          </HoverCardContent>
+        </HoverCard>
+      </div>
+      <div class="mr-5 mt-2">
+        <Avatar class="h-10 w-10">
+          <AvatarFallback>P</AvatarFallback>
+        </Avatar>
+      </div>
     </div>
   </nav>
 </template>
