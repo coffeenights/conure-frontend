a2424881ff627c4d24c3dbd9f79a4cd341fb024f
Oscar Lotero <oscarelotero@gmail.com>
2024-05-20 09:31:29 +0200

    WIP: Update API service error handling and add toast notification

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
-import { useUserStore } from '@/stores/UserStore'
 import axios, { AxiosInstance, AxiosResponse } from 'axios'
 import { getError } from './errors'
-import { toast } from '@/components/ui/toast'
+import { useUserStore } from '@/stores/UserStore'
+import { useToast } from '@/components/ui/toast'
+import { useRouter } from 'vue-router'
 
 const instance: AxiosInstance = axios.create({
   baseURL: import.meta.env.VITE_APP_API_URL,
@@ -15,12 +16,17 @@ instance.interceptors.response.use(
 
     if (error.response?.status === 401) {
       userStore.logout()
+    } else if (error.response?.status === 404) {
+      const router = useRouter()
+      router.push({ name: '404' })
+    } else {
+      const { toast } = useToast()
+      const errorMessage = getError(error)
+      toast({
+        title: 'Error',
+        description: errorMessage,
+      })
     }
-    const errorMessage = getError(error)
-    toast({
-      title: 'Error',
-      description: errorMessage,
-    })
     return Promise.reject(error)
   },
 )
