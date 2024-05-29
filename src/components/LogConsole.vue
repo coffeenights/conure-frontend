a24502ab1a4822a170e3c6dad13414db9cc250d9
mredvard <1638026+mredvard@users.noreply.github.com>
2024-05-29 15:40:02 +0800

    Add the possibility to select the logs from which pod should be streamed, also added a few visual changes

--- src/components/LogConsole.vue
+++ src/components/LogConsole.vue
@@ -1,61 +1,73 @@
 <script setup lang="ts">
-
-import {onMounted, ref} from "vue";
+import { ref, watch } from 'vue'
 
 const props = defineProps({
-  organizationId: String,
-  applicationId: String,
-  environment: String,
-  componentId: String,
+  organizationId: {
+    type: String,
+    required: true,
+  },
+  applicationId: {
+    type: String,
+    required: true,
+  },
+  environment: {
+    type: String,
+    required: true,
+  },
+  componentId: {
+    type: String,
+    required: true,
+  },
   pods: {
     type: Array as () => Array<string>,
-    required: true
-  }
+    required: true,
+  },
 })
 
 const logs = ref([] as string[])
 const logConsole = ref(null)
+let url = new URL(import.meta.env.VITE_APP_API_URL)
 
-onMounted(() => {
-  let url = new URL(import.meta.env.VITE_APP_API_URL)
-  url.pathname = `/organizations/${props.organizationId}/a/${props.applicationId}/e/${props.environment}/c/${props.componentId}/status/logs`
+let sources: EventSource[] = []
 
+url.pathname = `/organizations/${props.organizationId}/a/${props.applicationId}/e/${props.environment}/c/${props.componentId}/status/logs`
+
+const loadLogs = () => {
   let params = new URLSearchParams()
-
-// Add some parameters
-  let podsString = ''
-  for (let i = 0; i < props.pods.length; i++) {
-    podsString += props.pods[i]
-    if (i < props.pods.length - 1) {
-      podsString += ','
-    }
-  }
-  params.append('pods', podsString)
+  params.append('pods', props.pods.join(','))
   url.search = params.toString()
+  for (const s of sources) {
+    s.close()
+  }
 
+  logs.value = []
   const source = new EventSource(url.toString(), {
     withCredentials: true,
   })
 
-  source.onmessage = function(event) {
+  source.onmessage = function (event) {
     logs.value.push(event.data)
-  };
-
-// Define the onopen handler
-  source.onopen = function(event) {
-    console.log('Connection to server opened.')
-  };
-
-// Define the onerror handler
-  source.onerror = function(event) {
-    console.error('EventSource failed.')
   }
-})
 
+  // Define the onopen handler
+  source.onopen = function () {
+    sources.push(source)
+  }
+
+  // Define the onerror handler
+  source.onerror = function () {
+    source.close()
+  }
+}
+
+watch(() => props.pods, loadLogs, { immediate: true })
 </script>
 
 <template>
-  <div ref="logConsole" class="bg-background h-full grow overflow-y-scroll overflow-x-scroll p-3 scroller text-xs max-w-full rounded-sm dark:text-gray-300">
+  <div
+    ref="logConsole"
+    class="bg-background h-full grow overflow-y-scroll overflow-x-scroll p-3 scroller text-xs max-w-full rounded-sm dark:text-gray-300"
+  >
     <div v-for="log in logs" :key="log">
       {{ log }}
     </div>
@@ -77,5 +89,4 @@ onMounted(() => {
   overflow-anchor: auto;
   height: 1px;
 }
-
 </style>
--- src/views/applications/ComponentLogsTab.vue
+++ src/views/applications/ComponentLogsTab.vue
@@ -4,7 +4,7 @@ import { watch, ref } from 'vue'
 import { useBreadCrumbStore } from '@/stores/BreadCrumbStore'
 import { useRoute } from 'vue-router'
 import { Card, CardContent } from '@/components/ui/card'
-import {LogConsole} from "@/components";
+import { LogConsole } from '@/components'
 
 const route = useRoute()
 const breadCrumbStore = useBreadCrumbStore()
@@ -13,7 +13,6 @@ const isLoading = ref<boolean>(false)
 const notDeployed = ref<boolean>(false)
 const selectedPods = ref([] as string[])
 const emits = defineEmits(['podsLoaded'])
-
 
 const fetchData = () => {
   isLoading.value = true
@@ -25,9 +24,9 @@ const fetchData = () => {
   )
     .then((response) => {
       pods.value = response.data.pods
+      selectedPods.value = response.data.pods
       isLoading.value = false
       emits('podsLoaded')
-
     })
     .catch((error) => {
       if (error.response.data.code === '4004') {
@@ -40,28 +39,64 @@ const fetchData = () => {
 }
 watch(() => route.params.componentId, fetchData, { immediate: true })
 
+const handlePodClick = (pod: string) => {
+  if (selectedPods.value.includes(pod)) {
+    selectedPods.value = selectedPods.value.filter((p) => p !== pod)
+  } else {
+    selectedPods.value = [...selectedPods.value, pod]
+  }
+}
 
+const isPodSelected = (pod: string) => {
+  return selectedPods.value.includes(pod)
+}
 
+const isAllSelected = () => {
+  return selectedPods.value.length === pods.value.length
+}
+
+const handleAllPodsClick = () => {
+  if (selectedPods.value.length === pods.value.length) {
+    selectedPods.value = []
+  } else {
+    selectedPods.value = [...pods.value]
+  }
+}
 </script>
 
 <template>
   <div class="flex flex-row h-full gap-2">
-    <div class="flex flex-col gap-2">
-      <Card v-for="pod in pods" :key="pod">
-        <CardContent class="text-xs p-6">
+    <div class="flex flex-col gap-2 overflow-y-scroll">
+      <Card
+        class="cursor-pointer"
+        :class="{ 'bg-accent': isAllSelected() }"
+        @click="handleAllPodsClick"
+      >
+        <CardContent class="text-xs p-4 w-64 text-center">
+          Select all
+        </CardContent>
+      </Card>
+      <Card
+        v-for="pod in pods"
+        :key="pod"
+        class="cursor-pointer"
+        :class="{ 'bg-accent': isPodSelected(pod) }"
+        @click="handlePodClick(pod)"
+      >
+        <CardContent class="text-xs p-4 w-64 text-center">
           {{ pod }}
         </CardContent>
       </Card>
     </div>
     <div class="flex flex-col gap-2 h-full grow max-w-full overflow-x-hidden">
-        <LogConsole
-          v-if="!isLoading"
-          :organizationId="breadCrumbStore.organizationId"
-          :applicationId="breadCrumbStore.applicationId"
-          :environment="breadCrumbStore.environment"
-          :componentId="route.params.componentId as string"
-          :pods="pods"
-        />
+      <LogConsole
+        v-if="!isLoading"
+        :organization-id="breadCrumbStore.organizationId"
+        :application-id="breadCrumbStore.applicationId"
+        :environment="breadCrumbStore.environment"
+        :component-id="route.params.componentId as string"
+        :pods="selectedPods"
+      />
     </div>
   </div>
 </template>
