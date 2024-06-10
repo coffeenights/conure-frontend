48664d28f50726b7d977e74a1d51ac7cea6006bf
mredvard <1638026+mredvard@users.noreply.github.com>
2024-06-10 18:56:51 +0800

    Create a ComponentView to reuse the component window style i different views

--- src/views/applications/ComponentDetails.vue
+++ src/views/applications/ComponentDetails.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import TabNav from '@/components/TabNav.vue'
-import { Button } from '@/components/ui/button'
+import { ComponentView } from '@/components'
 
 const tabs = [
   { routeName: 'componentDetailsTab', caption: 'Details' },
@@ -11,22 +11,12 @@ const tabs = [
 const defaultTab: string = 'componentDetailsTab'
 </script>
 <template>
-  <div
-    id="componentDetails"
-    class="p-3 rounded-md border absolute top-0 left-0 w-full h-full bg-card overflow-y-scroll"
-  >
-    <div class="flex justify-end">
-      <router-link v-slot="{ navigate }" :to="{ name: 'componentList' }" custom>
-        <Button variant="ghost" size="icon" @click="navigate">
-          <span class="bi-x-lg"></span>
-        </Button>
-      </router-link>
-    </div>
+  <ComponentView>
     <div class="flex flex-col h-full">
       <TabNav :tabs="tabs" :default="defaultTab" />
       <div id="componentTabsContent" class="mt-4 h-full">
         <router-view></router-view>
       </div>
     </div>
-  </div>
+  </ComponentView>
 </template>
