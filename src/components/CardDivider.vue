001c5613a6b67818871b551331f3fbd6e7e43d9d
mredvard <1638026+mredvard@users.noreply.github.com>
2024-07-08 09:03:10 +0800

    Make the divider a component

---
+++ src/components/CardDivider.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+const props = defineProps({
+  title: String,
+})
+</script>
+
+<template>
+  <div class="py-3 border-b">
+    <div class="text-lg">{{ title }}</div>
+  </div>
+</template>
