32bff6b58367c54ff0d53950cd8fa21a28f1aaac
mredvard <1638026+mredvard@users.noreply.github.com>
2024-07-10 16:29:36 +0800

    Add a file for the github integration page

---
+++ src/components/GithubIntegration.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+const props = defineProps({
+  name: {
+    type: String,
+    default: 'World',
+  },
+})
+</script>
+
+<template>
+  <h1>Hello, {{ name }}!</h1>
+</template>
