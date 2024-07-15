1fa03f22046f78c5f971de95b98d3753dcecbf80
mredvard <1638026+mredvard@users.noreply.github.com>
2024-07-15 17:13:29 +0800

    Detect the darkmode and use the corresponding icons

--- src/components/ServiceIcon.vue
+++ src/components/ServiceIcon.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
-import { cn } from "@/utils";
+import { cn } from '@/utils'
+import { useDarkMode } from '@/composables/useDarkMode'
 
 const props = defineProps({
   name: String,
@@ -14,6 +15,8 @@ const props = defineProps({
   },
 })
 
+const { isDarkMode } = useDarkMode()
+
 type IconFile = {
   [key: string]: string
 }
@@ -26,6 +29,8 @@ const serviceIcon: IconFile = {
   static: 'FileImage.svg',
   mariadb: 'mariadb.svg',
   mysql: 'mysql.svg',
+  github: 'github.svg',
+  githubDark: 'github-dark.svg',
   raw: 'postgres.svg',
   redis: 'redis.svg',
   nodejs: 'nodejs.svg',
@@ -34,19 +39,26 @@ const serviceIcon: IconFile = {
   awsDark: 'aws_logo_dark.svg',
   awsRds: 'aws_rds.svg',
   awsRdsDark: 'aws_rds_dark.svg',
-
 }
 
 function getImageUrl(name: string) {
-  const icon = serviceIcon[name]
+  let icon: string
+  icon = serviceIcon[name]
+  if (isDarkMode) {
+    if (Object.prototype.hasOwnProperty.call(serviceIcon, `${name}Dark`)) {
+      icon = serviceIcon[`${name}Dark`]
+    }
+  }
   return new URL(`/src/assets/icons/${icon}`, import.meta.url).href
 }
 </script>
 
 <template>
-  <img :src="getImageUrl(name as string)" :alt="alt" :class="cn('w-10 h-12', props.class)" />
+  <img
+    :src="getImageUrl(name as string)"
+    :alt="alt"
+    :class="cn('w-10 h-12', props.class)"
+  />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
